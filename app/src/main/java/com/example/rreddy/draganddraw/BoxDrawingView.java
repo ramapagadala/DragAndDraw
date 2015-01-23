aabfe35e8423138ce75ac71b71bb85bb619c2400
Rama Pagadala <rama.pagadala@outlook.com>
2015-01-23 10:37:56 -0800

    Handling Touch Events - because you are subclassing View, you can override this View method: public boolean onTouchEvent(MotionEvent event)
    This method receives an instance of MotionEvent, a class that describes the touch event, including its location and its action. The action describes the stage of the event.
    Log a message in the case of each of the four different actions.

--- app/src/main/java/com/example/rreddy/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/example/rreddy/draganddraw/BoxDrawingView.java
@@ -1,19 +1,45 @@
 package com.example.rreddy.draganddraw;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by rreddy on 1/23/2015.
  */
 public class BoxDrawingView extends View {
+    public static final String TAG = "BoxDrawingView";
+
     // Used when creating the view in code
     public BoxDrawingView(Context context) {
         this(context, null);
     }
-    // Used when inflating the view from XML
+    // Used when inflating the view from layout XML
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF curr = new PointF(event.getX(), event.getY());
+        Log.i(TAG, "Received event at x=" + curr.x +
+                ", y=" + curr.y + ":");
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.i(TAG, " ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(TAG, " ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.i(TAG, " ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.i(TAG, " ACTION_CANCEL");
+                break;
+        }
+        return true;
     }
 }
