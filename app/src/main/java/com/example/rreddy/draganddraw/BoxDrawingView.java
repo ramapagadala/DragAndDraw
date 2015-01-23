97fb82ff00b2917898d23d3209fa57c5755385bb
Rama Pagadala <rama.pagadala@outlook.com>
2015-01-23 10:32:28 -0800

    created the new class: BoxDrawingView, made it a subclass of system's "View" You write two constructors because your view could be instantiated in code or from a layout file. Views instantiated from a layout file receive an instance of AttributeSet containing the XML attributes that were specified in XML. Even if you do not plan on using both constructors, it is good practice to include them.
    Next, updated fragment_drag_and_draw.xml layout file to use the new view.
    You must use BoxDrawingView’s fully qualified class name so that the layout inflater can find it. The inflater works through a layout file creating View instances

---
+++ app/src/main/java/com/example/rreddy/draganddraw/BoxDrawingView.java
@@ -0,0 +1,19 @@
+package com.example.rreddy.draganddraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by rreddy on 1/23/2015.
+ */
+public class BoxDrawingView extends View {
+    // Used when creating the view in code
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+    // Used when inflating the view from XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
